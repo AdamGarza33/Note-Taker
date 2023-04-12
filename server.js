8e65631d4eca9d1568eb6f29b1adfe558ac65984
Addit <adam.garza33@hotmail.com>
2023-04-12 00:45:18 -0500

    Require routes file and setup listener

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-// Import Express.js
+// Import Express.js dependency 
 const express = require('express');
 // 
 
 // Initialize express
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
+// Setup data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+//Require routes file
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+// Setup Listener
+app.listen(PORT, () => {
+    console.log(`App listening at localhost${PORT}`);
+  });
 
 
